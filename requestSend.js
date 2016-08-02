0aa9bfc69e903bdac9e0fd95d31c30732d004d99
CTKnoll <cta.knoll@gmail.com>
2016-08-01 23:30:27 -0400

    Ryan is a based God

--- requestSend.js
+++ requestSend.js
@@ -45,6 +45,18 @@ var getAuth = {
   user_id: "CTKnoll"
 };
 
+var getRyanInfo = {
+  format: 'json',
+  method: 'profile.get',
+  oauth_consumer_key: apiKey,
+  oauth_nonce: Math.random().toString(36).replace(/[^a-z]/g, '').substr(2),
+  oauth_signature_method: 'HMAC-SHA1',
+  oauth_timestamp: Math.floor(date.getTime() / 1000),
+  oauth_token: '121fcf7d265a48df94219a9aac64f47c',
+  oauth_version: '1.0',
+
+};
+
 var foodSearch = {
   format: 'json',
   method: 'foods.search',
@@ -53,15 +65,27 @@ var foodSearch = {
   oauth_signature_method: 'HMAC-SHA1',
   oauth_timestamp: Math.floor(date.getTime() / 1000),
   oauth_version: '1.0',
-  search_expression: encodeURIComponent("bread") // test query
+  search_expression: "bread" // test query
 };
 
+var userAuth = {
+  oauth_consumer_key: apiKey,
+  oauth_nonce: Math.random().toString(36).replace(/[^a-z]/g, '').substr(2),
+  oauth_signature_method: 'HMAC-SHA1',
+  oauth_timestamp: Math.floor(date.getTime() / 1000),
+  oauth_token: 'a89317f85f3d40e79b8e1d08204ec363',
+  oauth_verifier: '3870685',
+  oauth_version: '1.0'
+};
+
+//requestUserToken(requestID);
+//getAccessToken(userAuth);
 
 //callSearchAPI(foodSearch);
-//requestUserToken(requestID);
 //callGeneralAPI(genProfile);
-authenticate(getAuth);
-
+//authenticate(getAuth, editWeight, 100);
+//authenticate(getAuth, getProfile, null);
+callTokenAPI(getRyanInfo);
 
 function callSearchAPI(reqObj)
 {
@@ -91,7 +115,7 @@ function callSearchAPI(reqObj)
     data: reqObj
   }).on('complete', function(data, response) {
     //convert the data from XML to JSON format
-      setUpDetailedView(data);
+      getFoodInfo(data);
   });
 }
 
@@ -126,6 +150,41 @@ function callGeneralAPI(reqObj)
   }).on('complete', function(data, response) {
     //convert the data from XML to JSON format
     
+      console.log(data.food.servings.serving[0].calories);
+  });
+}
+
+function callTokenAPI(reqObj)
+{
+  // construct a param=value& string and uriEncode
+  var paramsStr = '';
+  for (var i in reqObj) {
+    paramsStr += "&" + i + "=" + reqObj[i];
+  }
+
+  // yank off that first "&"
+  paramsStr = paramsStr.substr(1);
+
+  var sigBaseStr = "POST&"
+                   + encodeURIComponent(fatSecretRestUrl)
+                   + "&"
+                   + encodeURIComponent(paramsStr);
+
+  // no  Access Token token (there's no user .. we're just calling foods.search)
+  sharedSecret2 = sharedSecret + "&" + '9c974fb87f534a5297a850d674b11432';
+
+  var hashedBaseStr  = crypto.createHmac('sha1', sharedSecret2).update(sigBaseStr).digest('base64');
+
+
+  // Add oauth_signature to the request object
+  reqObj.oauth_signature = hashedBaseStr;
+
+  // Launch!
+  rest.post(fatSecretRestUrl, {
+    data: reqObj
+  }).on('complete', function(data, response) {
+    //convert the data from XML to JSON format
+    
       console.log(data);
   });
 }
@@ -147,7 +206,8 @@ function callTokenAPI(reqObj, dataObj)
                    + encodeURIComponent(paramsStr);
 
   // no  Access Token token (there's no user .. we're just calling foods.search)
-  sharedSecret2 = sharedSecret + "&" + dataObj.profile.auth_secret.toString();
+  //sharedSecret2 = sharedSecret + "&" + dataObj.profile.auth_secret.toString();
+  sharedSecret2 = sharedSecret + "&" + '9c974fb87f534a5297a850d674b11432';
 
   var hashedBaseStr  = crypto.createHmac('sha1', sharedSecret2).update(sigBaseStr).digest('base64');
 
@@ -165,7 +225,7 @@ function callTokenAPI(reqObj, dataObj)
   });
 }
 
-function setUpDetailedView(data)
+function getFoodInfo(data)
 {
     var newDate = new Date;
     var foodObj = {
@@ -211,7 +271,7 @@ function requestUserToken(reqObj)
     data: reqObj
   }).on('complete', function(data, response) {
     //convert the data from XML to JSON format
-    userAuthorize(data);
+    console.log(data);
   });
 }
 
@@ -232,7 +292,40 @@ function userAuthorize(reqData)
     
 }
 
-function authenticate(reqObj)
+function getAccessToken(reqObj)
+{
+    var paramsStr = '';
+  for (var i in reqObj) {
+    paramsStr += "&" + i + "=" + reqObj[i];
+  }
+
+  // yank off that first "&"
+  paramsStr = paramsStr.substr(1);
+
+  var sigBaseStr = "POST&"
+                   + encodeURIComponent('http://www.fatsecret.com/oauth/access_token')
+                   + "&"
+                   + encodeURIComponent(paramsStr);
+
+  // no  Access Token token (there's no user .. we're just calling foods.search)
+  sharedSecret2 = sharedSecret + "&" + 'db87e81b060d4d3a8f3c022a259cc9aa';
+
+  var hashedBaseStr  = crypto.createHmac('sha1', sharedSecret2).update(sigBaseStr).digest('base64');
+
+
+  // Add oauth_signature to the request object
+  reqObj.oauth_signature = hashedBaseStr;
+
+  // Launch!
+  rest.post('http://www.fatsecret.com/oauth/access_token', {
+    data: reqObj
+  }).on('complete', function(data, response) {
+    //convert the data from XML to JSON format
+    console.log(data);
+  });
+}
+
+function authenticate(reqObj, callback, val)
 {
     // construct a param=value& string and uriEncode
   var paramsStr = '';
@@ -260,17 +353,37 @@ function authenticate(reqObj)
     data: reqObj
   }).on('complete', function(data, response) {
     //convert the data from XML to JSON format
-      getProfile(data);
+      data.customVal = val;
+      callback(data);
   });
 }
 
 function getProfile(data)
 {
     var newDate = new Date;
-    console.log(data.profile.auth_token);
     var profileFromID = {
     format: 'json',
     method: 'profile.get',
+    oauth_consumer_key: apiKey,
+    oauth_nonce: Math.random().toString(36).replace(/[^a-z]/g, '').substr(2),
+    oauth_signature_method: 'HMAC-SHA1',
+    oauth_timestamp: Math.floor(newDate.getTime() / 1000),
+    oauth_token: data.profile.auth_token.toString(),
+    oauth_version: '1.0'
+  };
+  callTokenAPI(profileFromID, data);
+}
+
+//current height and goal weight arent required, and require us to customize it for first time use
+function editWeight(data)
+{
+    var newDate = new Date;
+    var profileFromID = {
+    //current_height_cm: 200,
+    current_weight_kg: data.customVal,
+    format: 'json',
+    //goal_weight_kg: 100,
+    method: 'weight.update',
     oauth_consumer_key: apiKey,
     oauth_nonce: Math.random().toString(36).replace(/[^a-z]/g, '').substr(2),
     oauth_signature_method: 'HMAC-SHA1',
